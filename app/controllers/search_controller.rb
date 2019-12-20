3957bcc0eaabc0a29431c5bb8d14717d5c4b726c
Tyla Devon <tyladevon@gmail.com>
2019-12-20 10:15:47 -0700

    working on params for api call

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,13 @@
 class SearchController < ApplicationController
   def index
-
+    response = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1.json") do |request|
+      request.params['limit'] = '1'
+      request.params['api_key'] = ENV['nrel_key']
+      request.params['location'] = "17th St, Denver, CO, 80202"
+      request.params['radius'] = '5'
+      request.params['access_code'] = 'public'
+    end
+    results = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
   end
 end
