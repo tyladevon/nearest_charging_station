7e4e07989e516d2adf4f959922f1f3677deac1a8
Tyla Devon <tyladevon@gmail.com>
2019-12-20 10:36:07 -0700

    refactor Faraday api call, still not getting back narrowed down results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,17 @@
 class SearchController < ApplicationController
   def index
-    response = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1.json") do |request|
+    conn = Faraday.new(url: "https://developer.nrel.gov") do |f|
+      f.adapter  Faraday.default_adapter
+    end
+
+    response = conn.get('api/alt-fuel-stations/v1.json') do |request|
       request.params['limit'] = '1'
       request.params['api_key'] = ENV['nrel_key']
       request.params['location'] = "17th St, Denver, CO, 80202"
       request.params['radius'] = '5'
       request.params['access_code'] = 'public'
     end
+
     results = JSON.parse(response.body, symbolize_names: true)
     binding.pry
   end
